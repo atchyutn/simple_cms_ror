033cc3adeba08fad80c9e1ad532f19cc6a128b75
Atchyut Nagabhaiarava <atchyutnagabhairava@gmail.com>
2018-05-29 12:13:16 +0530

    added instance variables

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,11 +3,13 @@ class DemoController < ApplicationController
   layout false 
   
   def index
+    @array = [1,2,3,4]
     render('hello')
   end
 
   def hello
-    render('index')
+    #render('index')
+    @array = [1,2,3,4]
   end
 
   def other_hello
